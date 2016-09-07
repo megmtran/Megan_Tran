c8edb4ef5f2f1e561ec3219e30c7932d79220ce3
megmtran <TranM4917@sduhsd.lan>
2016-09-07 09:53:05 -0700

    added 02

---
+++ PyLesson_02/Lab_02.py
@@ -0,0 +1,16 @@
+num_01 = 5
+num_02 = 25
+product = (num_01 * num_02)
+print (num_01, "multiplied by" ,num_02, "is" ,product)
+
+name = "Megan Tran"
+address = "\n2555 Calle Mar de Mariposa"
+city = "\nSan Diego"
+zip = "\n92130"
+print (name + address + city + zip)
+
+l = 2
+w = 6
+h = 13
+sa = 2 * (l * w + l * h + w * h)
+print ("The surface area of your rectangle is" ,sa)
---
+++ PyLesson_02/usingvariables.py
@@ -0,0 +1,2 @@
+name = "Megan Tran"
+print (name)
