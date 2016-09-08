9445e9f15161386e0dfa15d956a47137bbf4a00a
megmtran <TranM4917@sduhsd.lan>
2016-09-08 10:25:35 -0700

    added lesson_03

---
+++ PyLesson_03/RudeAI.py
@@ -0,0 +1,7 @@
+print ("Hi, my name is RudeAI.")
+print ("I\'d like to ask you a few questions.")
+
+name = input ("What is your name? ")
+print (name , "?!!! Why would anyone name a baby that?")
+age = input ("How old are you," ,name,"?")
+
