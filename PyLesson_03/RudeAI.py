efcf34642e08dfde2885fd6accdc5efce1b36ac7
megmtran <TranM4917@sduhsd.lan>
2016-09-12 09:39:19 -0700

    saving AI

--- PyLesson_03/RudeAI.py
+++ PyLesson_03/RudeAI.py
@@ -3,5 +3,9 @@ print ("I\'d like to ask you a few questions.")
 
 name = input ("What is your name? ")
 print (name , "?!!! Why would anyone name a baby that?")
-age = input ("How old are you," ,name,"?")
+age = input ("How old are you, " ,name, "?")
+print ("Ooooo!!!" ,age, "is getting up there.")
+hobby = input ("What do you do for fun," ,name, "?")
+print ("I thought only nerds like to" , hobby)
+music = input ("
 
