640ecf1c12f69679a4e93335f4ed94cabf643dc7
megmtran <megmtran@yahoo.com>
2016-09-12 10:23:02 -0700

    saved changes

--- PyLesson_03/RudeAI.py
+++ PyLesson_03/RudeAI.py
@@ -3,9 +3,16 @@ print ("I\'d like to ask you a few questions.")
 
 name = input ("What is your name? ")
 print (name , "?!!! Why would anyone name a baby that?")
-age = input ("How old are you, " ,name, "?")
-print ("Ooooo!!!" ,age, "is getting up there.")
-hobby = input ("What do you do for fun," ,name, "?")
-print ("I thought only nerds like to" , hobby)
-music = input ("
-
+age = input ("How old are you, " + name + "? ")
+print ("Ooooo!!!" , age , "is getting up there.")
+hobby = input ("What do you do for fun, " + name + "? ")
+print ("I thought only nerds like to" , hobby , ".")
+music = input ("What kind of music do you like? ")
+print ("Boooo!!! I wouldn\'t wish the sound of" , music , "on my worst enemy.")
+siblings = int (input ("How many siblings do you have? "))
+print (siblings , "? Wow, I hope the rest of your family is better looking.")
+future = input ("What do you want to be when you grow up? ")
+print ("I think you\'d have to be smarter to be a" , future , ".")
+print ("So" , name , ", you\'re" , age , "... \n You like to" , hobby , "and listen to" , music , "... \n Good luck becoming a" , future , ". \n I'm only kiddin\'" , name , ".")
+                
+                  
