25cfb68d10b989d47f65ef7beadb620699b63505
megmtran <megmtran@yahoo.com>
2016-09-13 09:48:28 -0700

    added labs

---
+++ PyLesson_03/ex_02.py
@@ -0,0 +1,4 @@
+height = int (input ("What is your height in inches? "))
+weight = int (input ("What is your weight in pounds? "))
+bmi = 703 * weight / (height * height)
+print ("Your BMI is" , bmi , ".")
---
+++ PyLesson_03/ex_03.py
@@ -0,0 +1,6 @@
+print ("In this program, the density of a liquid will be calculated.")
+mass = int (input ("In grams, what is the mass of the liquid? "))
+volume = int (input ("In milliliters, what is the volume of the liquid? "))
+density = mass / volume
+print ("The density of the liquid is" , density , "g/mL.")
+
