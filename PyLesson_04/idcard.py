e9ddbe35f1a5d0eddff731c7e20a7323d64bda55
megmtran <megmtran@yahoo.com>
2016-09-20 10:04:07 -0700

    saved lesson_04 labs

--- PyLesson_04/idcard.py
+++ PyLesson_04/idcard.py
@@ -1,8 +1,15 @@
-def idcard (first, last, title, school, year, subject)
-
+def idcard (item1, item2):
+    print ("*" , "{:<17}\t{:>17}".format (item1, item2) , "*")
+    
 first = input ("Enter your first name: ")
 last = input ("Enter your last name: ")
 title = input ("Enter your title: ")
 school = input ("Enter the school site: ")
 year = input ("Enter the school year: ")
 subject = input ("What is your subject? ")
+
+print ("*******************************************")
+idcard (school, year)
+idcard (first, last)
+idcard (title, subject)
+print ("*******************************************")
--- PyLesson_04/reciept.py
+++ PyLesson_04/reciept.py
@@ -1,24 +1,22 @@
-def receipt(item1, price1, item2, price2, item3, price3):
-    print ("*" , "{:.>10}\t{:10.2f}".format (item1 , price1))
-    print ("*" , "{:.>10}\t{:10.2f}".format (item2 , price2))
-    print ("*" , "{:.>10}\t{:10.2f}".format (item3 , price3))
-    print ("*" , "{:.>10}\t{10.2f}".format ("Subtotal:" , subtotal))
-    print ("*" , "{:.>10}\t{10.2f}".format ("Tax:" , tax))
-    print ("*" , "{:.>10}\t{10.2f}".format ("Total:" , total))
-           
+def receipt(item, price):
+    print ("*" , "{:>10} ........\t{:10.2f}".format (item, price))
+              
 item1 = input ("Please enter item one: ")
-price1 = int(input ("Please enter the price: "))
+price1 = float(input ("Please enter the price: "))
 item2 = input ("Please enter item two: ")
-price2 = int(input ("Please enter the price: "))
+price2 = float(input ("Please enter the price: "))
 item3 = input ("Please enter item three: ")
-price3 = int(input ("Please enter the price"))
+price3 = float(input ("Please enter the price: "))
 subtotal = price1 + price2 + price3
 tax = 0.08 * subtotal
 total = subtotal + tax
 
 print ("<<<<<<<<<<<<<<<__Receipt__>>>>>>>>>>>>>>>>")
-
-receipt (item1, price1, item2, price2, item3, price3)
-
+receipt (item1, price1)
+receipt (item2, price2)
+receipt (item3, price3)
+receipt ("Subtotal:", subtotal)
+receipt ("Tax:", tax)
+receipt ("Total:", total)
 print ("_______________________________________")
 print ("* Thank you for your support *")
