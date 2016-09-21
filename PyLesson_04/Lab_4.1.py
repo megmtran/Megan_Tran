ac3e0779526a678d64cca5d470a9dfaa96ae0406
megmtran <megmtran@yahoo.com>
2016-09-21 10:25:27 -0700

    added lesson 04.1 lab

---
+++ PyLesson_04/Lab_4.1.py
@@ -0,0 +1,11 @@
+def loan (r, p, n, t):
+    a = p * (1 + r / n)**(n*t)
+    return ("${:4.2f}".format (a))
+
+r = float (input ("Enter the interest rate: "))
+p = float (input ("Enter the principal amount: "))
+n = float (input ("Enter the number of times the loan is compounded per year: "))
+t = float (input ("Enter the life of the loan in years: "))
+
+print (loan (r, p, n, t))
+
