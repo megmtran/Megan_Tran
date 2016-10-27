cec3440c386083e5d3c2dba2578ca9ab4b7036a1
megmtran <megmtran@yahoo.com>
2016-10-27 10:01:41 -0700

    completed lab 6.1

--- PyLesson_06/Song.py
+++ PyLesson_06/Song.py
@@ -2,11 +2,17 @@ w1 = "Na"
 w2 = "Hey"
 w3 = "Goodbye!"
 
-def printLine (word):
-    for i in range ():
-        print (word)
+def printLine (word, repeat):
+    strng=""
+    for i in range (0, repeat):
+        strng = strng + word + " "
+    print(strng)
+        
+printLine (w1,4)
+printLine (w1,4)
+printLine (w2,3)
+printLine (w3,1)
 
-printLine (w1)
-printLine (w1)
-printLine (w2)
-printLine (w3)
+
+
+
