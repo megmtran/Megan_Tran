bdb113b572fc2673480cceb4aeff68d095e5c475
megmtran <megmtran@yahoo.com>
2016-11-15 09:34:26 -0800

    fixed lab 6

--- PyLesson_06/Factorial.py
+++ PyLesson_06/Factorial.py
@@ -1,9 +1,11 @@
 number = int(input("Enter a number: "))
 fact = 1
 
-for i in range(1,number):
-    print (i*fact)
+for i in range(1,number+1):
+    fact = i * fact
+print (fact)
              
+
 
 
 
