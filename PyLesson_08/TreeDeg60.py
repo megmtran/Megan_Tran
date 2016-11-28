250b91dd27d422c102afaf858a867845f1457493
megmtran <TranM4917@sduhsd.lan>
2016-11-28 10:19:56 -0800

    updated lesson_08

--- PyLesson_08/TreeDeg60.py
+++ PyLesson_08/TreeDeg60.py
@@ -3,8 +3,7 @@ stop = len(wd)
 start = 0
 def tree(wd,start,stop):
     if start <= stop:
-        print (wd[0:start])
+        print ("{:>15}".format(wd[0:start]))
         start += 1
         tree(wd,start,stop)
-
-print ("{:>10}").format(tree(wd,start,stop))
+tree(wd,start,stop)
