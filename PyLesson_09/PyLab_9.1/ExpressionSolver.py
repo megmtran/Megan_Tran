5c757d25971407d5bd534326eeb8a858841b591a
megmtran <TranM4917@sduhsd.lan>
2016-12-16 10:24:43 -0800

    updated lab_09

--- PyLesson_09/PyLab_9.1/ExpressionSolver.py
+++ PyLesson_09/PyLab_9.1/ExpressionSolver.py
@@ -1,2 +1,27 @@
 exn = int(input("Enter a mathenatical expression: "))
-equation = exn.split()
+equation = exn.split(" ")
+i = 0
+while i < len(equation):
+    if i < len(equation) and (equation[i]=="*" or equation[i]="/"):
+        if equation[i]="*":
+            equation[i] = int(equation[i-1])*int(equation[i+1])
+        else:
+            equation[i] = int(equation[i-1])/int(equation[i+1])
+        equation.pop(i-1)
+        equation.pop(i)
+    else:
+        i +=1
+i=0
+while i<len(equation):
+    if i<len(equation) and (equation[i]="+" or equation[i]="-"):
+        if equation[i]="+":
+            equation[i] = int(equation[i-1])+int(equation[i+1])
+        else:
+            equation[i] = int(equation[i-1])-int(equation[i+1])
+        equation.pop(i-1)
+        equation.pop(i)
+    else:
+        i +=1
+print(equation)
+    
+        
