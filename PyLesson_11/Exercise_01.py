d0ac48d8344e80ba370fa7c92b66dfa2a4fcdab8
megmtran <TranM4917@sduhsd.lan>
2017-01-13 10:24:29 -0800

    updated lab_11

---
+++ PyLesson_11/Exercise_01.py
@@ -0,0 +1,31 @@
+class MilesPerHour:
+    def__init__(self,distance,hours,minutes):
+        self.dist = distance
+        self.hrs = hours
+        self.mins = minutes
+        mph = 0
+
+    def setValues(self,dis,hr,mns):
+        self.dist = dis
+        self.hrs = hr
+        self.mins = mns
+        mph = 0
+
+    def getDist():
+        return self.dist
+    def getHours():
+        return self.hrs
+    def getMins():
+        return self.mins
+    def getMPH():
+        mph = dist/(hrs+mins/60.0)
+        return mph
+
+def main():
+    dist = input("Enter a distance value: ")
+    hrs = input("Enter a value for hours: ")
+    mins = input("Enter a value for minutes: ")
+
+    values1 = MilesPerHour(dist,hrs,mins)
+
+    print(values1.getDist(),"miles in",hrs+mins/60.0,"hours =",values1.getMPH)
