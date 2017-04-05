db9be9eb83b47fc6153a0681aed2b11e5ed9a0ed
megmtran <TranM4917@sduhsd.lan>
2017-04-05 10:25:23 -0700

    updated WebLab_14

--- WebLab_14/LabScript.js
+++ WebLab_14/LabScript.js
@@ -1,13 +1,19 @@
-function valUser() {
-	var x = document.forms.input.username.value;
-	var atPos = x.indexOf("@");
-	var dotPos = x.lastIndexOf(".");
+var x = document.forms.input.username.value;
+var atPos = x.indexOf("@");
+var dotPos = x.lastIndexOf(".");
+var user = ""
+var pass = ""
+
+validate();
+
+function valUser()
+{
 	if(atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > x.length)
-		var user = "f";
+		user = "f";
 	else
-		var user = "t";
-	valPass();
+		user = "t";
 }
+
 function valPass() {
 	var passL = document.forms.input.passwd.length;
 	if(passL < 6)
@@ -15,14 +21,21 @@ function valPass() {
 	else
 		var pass = "t";
 }
-function validate() {
-	valUser();	
-	if(user = "t" || pass = "t")
-		alert("Success");
-	if(user = "f" || pass = "f")
+
+function validate() 
+{
+	valUser();
+	valPass();
+	
+	if(user = "f" && pass = "f")
 		alert("We were unable to process your request. Please enter a valid email address and a password of at least 6 characters.");
-	if(user = "f" || pass = "t")
-		alert("We were unable to process your request. Please enter a valid email address.");
-	if(user = "t" || pass = "f")
-		alert("We were unable to process your request. Please enter a password of at least 6 characters.");
+	else
+	{
+		if(user = "f")
+			alert("We were unable to process your request. Please enter a valid email address.");
+		if(pass = "f")
+			alert("We were unable to process your request. Please enter a password of at least 6 characters.");
+		else
+			alert("Success!");
+	}
 }
